5c2afecc6adb679e6924541201dd499725783eb3
gilberto-009199 <gilberto.tec@vivaldi.net>
2023-09-05 16:26:46 -0300

    deploy mvn

--- src/main/java/com/voyager/enterprise/people/usercase/CasePayment.java
+++ src/main/java/com/voyager/enterprise/people/usercase/CasePayment.java
@@ -1,6 +1,7 @@
 package com.voyager.enterprise.people.usercase;
 
+import com.voyager.enterprise.people.entity.PaymentEmployee;
 import com.voyager.enterprise.people.entity.PaymentService;
 import com.voyager.enterprise.usercase.CaseGeneric;
 
-public interface CasePayment  extends CaseGeneric<PaymentService> {}
+public interface CasePayment  extends CaseGeneric<PaymentEmployee> {}
