7a64f755ac67fda912ba695e404bffaacd7e5025
gilberto-009199 <gilberto.tec@vivaldi.net>
2023-09-09 16:43:29 -0300

    refactor: annotation deriv

--- src/main/java/com/voyager/enterprise/event/EventHandler.java
+++ src/main/java/com/voyager/enterprise/event/EventHandler.java
@@ -5,8 +5,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
+
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
     public String eventName() default "*";
 }
--- src/main/java/com/voyager/enterprise/financial/event/CreateBook.java
+++ src/main/java/com/voyager/enterprise/financial/event/CreateBook.java
@@ -1,4 +1,11 @@
 package com.voyager.enterprise.financial.event;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import com.voyager.enterprise.event.EventHandler;
+
+@EventHandler(eventName = "com.voyager.enterprise.financial.event.CreateBook")
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CreateBook {
 }
