25c803cccf61a9fe221b80458b5ec70345ad4d3f
gilberto-009199 <gilberto.tec@vivaldi.net>
2023-09-09 14:34:03 -0300

    Retencion annotation test

--- src/main/java/com/voyager/enterprise/event/CreateEnterprise.java
+++ src/main/java/com/voyager/enterprise/event/CreateEnterprise.java
@@ -1,9 +1,9 @@
 package com.voyager.enterprise.event;
 
-import java.lang.annotation.ElementType;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @EventHandler(eventName = "com.voyager.enterprise.event.CreateEnterprise")
+@Retention(RetentionPolicy.RUNTIME)
 public @interface CreateEnterprise {}
